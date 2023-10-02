49c93306ae9d52293142592d22644bc901287dc8
trudypainter <140916783+sahilvers@users.noreply.github.com>
2023-10-02 18:22:04 -0400

    whats on the app store before camera switching for stoddard

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { SafeAreaView, View, StyleSheet } from "react-native";
 import { Text } from "react-native";
 import {
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
+  TapGestureHandler,
+  State,
 } from "react-native-gesture-handler";
 import Svg, { Line, Rect } from "react-native-svg";
+import { Image } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import ViewShot from "react-native-view-shot";
 
 export default function App() {
   const [tGestureStart, setTGestureStart] = useState("");
@@ -15,54 +20,36 @@ export default function App() {
   const [tGestureEnd, setTGestureEnd] = useState("");
 
   const [rects, setRects] = useState([]);
+  const [capturedImage, setCapturedImage] = useState(null); // New state variable
+
+  const [cameraPosition, setCameraPosition] = useState(null);
+  const cameraRef = useRef(null); // Ref for the Camera component
+
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  const safeAreaViewRef = useRef(null);
+
+  useEffect(() => {
+    if (!permission) {
+      requestPermission();
+    }
+  }, [permission]);
+
+  const onDoubleTap = (event) => {
+    if (event.nativeEvent.state === State.ACTIVE) {
+      console.log("double tap detected");
+      // remove most recent rect
+      const newRects = rects.slice(0, rects.length - 1);
+      setRects(newRects);
+    }
+  };
 
   const pan = Gesture.Pan()
     .onStart((g) => {
-      // Start gesture looks like this
-      // {
-      //   "absoluteX": 178,
-      //   "absoluteY": 484,
-      //   "handlerTag": 2,
-      //   "numberOfPointers": 1,
-      //   "oldState": 2,
-      //   "state": 4,
-      //   "target": 115,
-      //   "translationX": -4.3333282470703125,
-      //   "translationY": 0,
-      //   "velocityX": -172.0102558333448,
-      //   "velocityY": 0,
-      //   "x": 178,
-      //   "y": 484,
-      // }
       setTGestureStart(`${Math.round(g.x)}, ${Math.round(g.y)}`);
     })
     .onTouchesMove((g) => {
-      // Move gesture looks like this
-      // {
-      //   "allTouches": Array [
-      //     {
-      //       "absoluteX": 123.33332824707031,
-      //       "absoluteY": 449,
-      //       "id": 0,
-      //       "x": 123.33332824707031,
-      //       "y": 449,
-      //     },
-      //   ],
-      //   "changedTouches": Array [
-      //    {
-      //       "absoluteX": 123.33332824707031,
-      //       "absoluteY": 449,
-      //       "id": 0,
-      //       "x": 123.33332824707031,
-      //       "y": 449,
-      //     },
-      //   ],
-      //   "eventType": 2,
-      //   "handlerTag": 2,
-      //   "numberOfTouches": 1,
-      //   "state": 4,
-      //   "target": 115,
-      // }
       setTGestureMove(
         `${Math.round(g.changedTouches[0].x)}, ${Math.round(
           g.changedTouches[0].y
@@ -70,94 +57,133 @@ export default function App() {
       );
     })
     .onUpdate((g) => {
-      // Update gesture looks like
-      // {
-      //   "absoluteX": 229,
-      //   "absoluteY": 400.3333282470703,
-      //   "handlerTag": 2,
-      //   "numberOfPointers": 1,
-      //   "state": 4,
-      //   "target": 115,
-      //   "translationX": 0,
-      //   "translationY": 1.6666717529296875,
-      //   "velocityX": 0,
-      //   "velocityY": 24.111687246989227,
-      //   "x": 229,
-      //   "y": 400.3333282470703,
-      // }
-      setTGestureUpdate(`${Math.round(g.x)}, ${Math.round(g.y)}`);
+      requestAnimationFrame(() => {
+        setTGestureUpdate(`${Math.round(g.x)}, ${Math.round(g.y)}`);
+
+        let startX = parseFloat(tGestureStart.split(",")[0]);
+        let startY = parseFloat(tGestureStart.split(",")[1]);
+        let endX = Math.round(g.x);
+        let endY = Math.round(g.y);
+
+        // handle the case where the user drags from right to left
+        if (endX < startX) {
+          endX = startX;
+          startX = Math.round(g.x);
+        }
+
+        // handle the case where the user drags from bottom to top
+        if (endY < startY) {
+          endY = startY;
+          startY = Math.round(g.y);
+        }
+
+        const width = endX - startX;
+        const height = endY - startY;
+
+        const cameraSize = { width, height };
+        const cameraPos = {
+          x: startX,
+          y: startY,
+          ...cameraSize,
+        };
+        setCameraPosition(cameraPos);
+      });
     })
     .onEnd((g) => {
-      // End gesture looks like this
-      // {
-      //   "absoluteX": 213.3333282470703,
-      //   "absoluteY": 542.6666564941406,
-      //   "handlerTag": 2,
-      //   "numberOfPointers": 0,
-      //   "oldState": 4,
-      //   "state": 5,
-      //   "target": 115,
-      //   "translationX": -66,
-      //   "translationY": 172,
-      //   "velocityX": -71.28075141720757,
-      //   "velocityY": 567.0058445795321,
-      //   "x": 213.3333282470703,
-      //   "y": 542.6666564941406,
-      // }
       setTGestureStart(null);
       setTGestureEnd(`${Math.round(g.x)}, ${Math.round(g.y)}`);
-      const startX = parseFloat(tGestureStart.split(",")[0]);
-      const startY = parseFloat(tGestureStart.split(",")[1]);
-      const endX = Math.round(g.x);
-      const endY = Math.round(g.y);
+
+      let startX = parseFloat(tGestureStart.split(",")[0]);
+      let startY = parseFloat(tGestureStart.split(",")[1]);
+      let endX = Math.round(g.x);
+      let endY = Math.round(g.y);
+
+      // handle the case where the user drags from right to left
+      if (endX < startX) {
+        endX = startX;
+        startX = Math.round(g.x);
+      }
+
+      // handle the case where the user drags from bottom to top
+      if (endY < startY) {
+        endY = startY;
+        startY = Math.round(g.y);
+      }
 
       const width = endX - startX;
       const height = endY - startY;
 
-      setRects([...rects, { x: startX, y: startY, width, height }]);
+      // Capture the image from the camera
+      cameraRef.current
+        .takePictureAsync()
+        .then((photo) => {
+          console.log("startX", startX, "startY", startY);
+          // Add the captured image to the list of rects
+          setRects([...rects, { x: startX, y: startY, width, height, photo }]);
+        })
+        .catch((error) => {
+          console.log("Error capturing image:", error);
+          setCameraPosition(null);
+        });
+
+      setCameraPosition(null);
     });
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <GestureDetector gesture={pan}>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-          <View style={StyleSheet.absoluteFill}>
-            <Svg style={StyleSheet.absoluteFill}>
-              {rects.map((rect, index) => (
-                <Rect
-                  key={index}
-                  x={rect.x}
-                  y={rect.y}
-                  width={rect.width}
-                  height={rect.height}
-                  stroke="white"
-                  strokeWidth="2"
-                  fill="transparent" // adjust as needed
-                />
-              ))}
-
-              {tGestureStart && (
-                <Rect
-                  x={Math.min(
-                    tGestureStart.split(",")[0],
-                    tGestureMove.split(",")[0]
-                  )}
-                  y={Math.min(
-                    tGestureStart.split(",")[1],
-                    tGestureMove.split(",")[1]
-                  )}
-                  width={Math.abs(
-                    tGestureStart.split(",")[0] - tGestureMove.split(",")[0]
-                  )}
-                  height={Math.abs(
-                    tGestureStart.split(",")[1] - tGestureMove.split(",")[1]
-                  )}
-                  stroke="white"
-                  strokeWidth="2"
-                  fill="transparent" // adjust as needed
-                />
-              )}
-            </Svg>
-          </View>
+      <TapGestureHandler onHandlerStateChange={onDoubleTap} numberOfTaps={2}>
+        <View style={{ flex: 1 }}>
+          <GestureDetector gesture={pan}>
+            <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+              <View style={StyleSheet.absoluteFill}>
+                {rects.map((rect, index) => (
+                  <Image
+                    source={{ uri: rect.photo.uri }}
+                    style={{
+                      position: "absolute",
+                      left: rect.x,
+                      top: rect.y,
+                      width: rect.width,
+                      height: rect.height,
+                      backgroundColor: "black",
+                    }}
+                  />
+                ))}
+                {cameraPosition && (
+                  <Camera
+                    ref={cameraRef} // Add the ref to the Camera component
+                    style={{
+                      position: "absolute",
+                      left: cameraPosition.x,
+                      top: cameraPosition.y,
+                      width: cameraPosition.width,
+                      height: cameraPosition.height,
+                      borderColor: "black",
+                      borderWidth: 2,
+                      borderStyle: "solid",
+                    }}
+                    type={type}
+                  />
+                )}
+              </View>
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 24,
+                  fontWeight: "bold",
+                  position: "absolute",
+                  left: 12,
+                  top: 50,
+                  padding: 10,
+                  paddingLeft: 14,
+                  paddingRight: 14,
+                  backgroundColor: "blue",
+                }}
+              >
+                SLINGSHOT
+              </Text>
+              {/* 
+          ****** FOR DEBUGGING ****
           <Text
             style={{ color: "white", fontSize: 24 }}
           >{`Gesture started at:  ${tGestureStart}`}</Text>
@@ -165,13 +191,15 @@ export default function App() {
             style={{ color: "white", fontSize: 24 }}
           >{`Gesture moved to:  ${tGestureMove}`}</Text>
           <Text
-            style={{ color: "white", fontSize: 24 }}
+            style={{ color: "yellow", fontSize: 24 }}
           >{`Gesture updated to:  ${tGestureUpdate}`}</Text>
           <Text
             style={{ color: "white", fontSize: 24 }}
-          >{`Gesture ended at:  ${tGestureEnd}`}</Text>
-        </SafeAreaView>
-      </GestureDetector>
+          >{`Gesture ended at:  ${tGestureEnd}`}</Text> */}
+            </SafeAreaView>
+          </GestureDetector>
+        </View>
+      </TapGestureHandler>
     </GestureHandlerRootView>
   );
 }
